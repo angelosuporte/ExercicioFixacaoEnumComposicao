6a86c8c529c6866cb532870fa331911405699320
Angelo Carvalho <angelosuporteti@gmail.com>
2020-07-09 12:32:52 -0300

    Implementação da classe ItemPedido

--- ExercicioFixacaoEnumComposicao/Entidades/Produto.cs
+++ ExercicioFixacaoEnumComposicao/Entidades/Produto.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ExercicioFixacaoEnumComposicao.Entidades
+﻿namespace ExercicioFixacaoEnumComposicao.Entidades
 {
     class Produto
     {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
     }
 }
