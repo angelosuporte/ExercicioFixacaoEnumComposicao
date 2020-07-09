a5230d393ab1fdef6526761034c3996e781c210c
Angelo Carvalho <angelosuporteti@gmail.com>
2020-07-09 14:00:18 -0300

    Implementação da classe Cliente

--- ExercicioFixacaoEnumComposicao/Entidades/ItemPedido.cs
+++ ExercicioFixacaoEnumComposicao/Entidades/ItemPedido.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ExercicioFixacaoEnumComposicao.Entidades;
 
 namespace ExercicioFixacaoEnumComposicao.Entidades
 {
     class ItemPedido
     {
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+        public double Preco { get; set; }
+        
+
+        public ItemPedido()
+        {
+        }
+
+        public ItemPedido(Produto produto, int quantidade, double preco)
+        {
+            Produto = produto;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+
+        public double subTotal(double subtotal)
+        {
+            return Quantidade * Preco;
+        }
     }
 }
